3bdff4e3bb6d12e211d528b274fdaa6261ad22a8
soest <felix.soest@vatger.de>
2025-10-12 18:22:51 +0200

    Add theory roster implementation

--- monitor/core_requests.py
+++ monitor/core_requests.py
@@ -24,7 +24,7 @@ eud_header = {
     'User-Agent': 'VATGER'
 }
 
-@cached(cache=TTLCache(maxsize=float('inf'), ttl=60*5))
+# @cached(cache=TTLCache(maxsize=float('inf'), ttl=60*5))
 def get_theory_roster() -> list[int]:
     url = "http://docker.vatsim-germany.org:8016/roster/"
     s1_header = {
